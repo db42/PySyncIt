99efb65cf27932c0bfed7f40b682a0d761e4a493
Dushyant Bansal <dushyant37@gmail.com>
2012-08-11 12:04:54 +0530

    very minor changes- blank spaces and comments.

--- monitor.py
+++ monitor.py
@@ -8,6 +8,7 @@ from server import Server, ClientData
 from client import Client
 
 class FileData(object):
+
     def __init__(self, file_name, time, owner):
         self.name = file_name
         self.time = time
@@ -15,12 +16,12 @@ class FileData(object):
 
 
 def setup_logging():
-    #Start Logging
     logger = logging.getLogger('syncIt')
     #handler = logging.FileHandler('/tmp/syncIt.log')
     handler = logging.StreamHandler()
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)
+
 
 def ensure_dir(dirs, user_name):
     """
@@ -31,30 +32,33 @@ def ensure_dir(dirs, user_name):
         if not os.path.isdir(my_dir):
             os.makedirs(my_dir)
 
+
 def main():
     #use argparse to get role, ip, uname
     parser = argparse.ArgumentParser(
-        description="""SyncIT""",
+        description="""PySyncIt""",
         formatter_class=argparse.RawDescriptionHelpFormatter)
     
     parser.add_argument(
-        '-ip', help='Specify the ipaddr of this machine')
+        '-ip', help='Specify the ip address of this machine')
 
     parser.add_argument(
-        '-port', help='Specify the port of this machine')
+        '-port', help='Specify the port of this machine to run rpc server')
 
     parser.add_argument(
         '-uname', help='Specify the user name of this machine')
     
     parser.add_argument(
-        '-role', help='Specify the role of this machine')
+        '-role', help='Specify the role of this machine - client or server')
     
     args = parser.parse_args()
 
+    #start logging
     setup_logging()
     logger = logging.getLogger('syncIt')
     logger.info('Logging started')
 
+    #get parameters from config file
     config = ConfigParser.ConfigParser()
     config.read('syncit.cfg')
 
